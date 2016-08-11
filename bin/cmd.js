0a51e3b2f6d94c9c9ca0ee88924f0330e8afca78
itnik <997585659@qq.com>
2016-08-11 15:36:50 +0800

    修正找不到文件的错误

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 /**
  * 依赖
  */
--- index.js
+++ index.js
@@ -14,38 +14,27 @@ function generator(pwd, source_file_name, dest_file_path, is_open, options) {
 		if(is_debug == true)
 			console.log(str);
 	}
-  
 	var str = fs.realpathSync('.'); 
-
-	log(str);  
-	
 	var pwd = pwd
-
 	var preview_path = pwd + '/preview';
-	
 	var source_file_path = source_file_name;
-	
 	var dest_file_path = dest_file_path ;
-	
 	if (test('-d', preview_path)) { 
 		mkdir('-p', preview_path);
 	};
-	
 	cp_template_dir(__dirname, preview_path);
 	_toc_config(__dirname, preview_path);
-	
 	function cp_template_dir(_cur_dir,_dest_dir){
 		var i = _cur_dir;
 		log(i);
 	
 		cp('-R', _cur_dir +'/vendor/toc', _dest_dir+'/');
 	}
-	
   	function _toc_config(_cur_dir,_dest_dir){
-		if (test('-d', _dest_dir + "/toc_conf.js")) { 
-			log('toc_conf file exist')
+		if (test('-d', _dest_dir + "/config.js")) { 
+			log('config file exist')
 		}else{
-     cp('-R', _cur_dir +'/vendor/toc_conf.js', _dest_dir+'/');
+     cp('-R', _cur_dir +'/vendor/config.js', _dest_dir+'/');
 		}
   	}
 	
@@ -66,19 +55,13 @@ function generator(pwd, source_file_name, dest_file_path, is_open, options) {
 		rs.on("end", function () {
 			var source = bufferHelper.toBuffer().toString('utf8');
 			var template = Handlebars.compile(source);
-		
-			log(template);
-		
 			var	marked = require('marked');	
-
 			marked(data.toString(), options, function (err, data) {
 				if (err) {
 					log('err ' + err);
 					return;
 				}
-				log(data);
-			
-				var css_link = "ddsds";
+
 				var content = {
 					"title":_file_name,
 					"parse_markdown": data
@@ -87,7 +70,6 @@ function generator(pwd, source_file_name, dest_file_path, is_open, options) {
 				var final_html_content = new Buffer( template(content) );
 				fs.writeFile(dest_file_path, final_html_content , function (err) {
 				  if (err) throw err;
-				  log('It\'s saved!');
 					if(is_open == true){
 						open(dest_file_path);
 					}
