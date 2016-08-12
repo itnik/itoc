fa084f95ce9a746317dc5e675a2226e3f0ae3288
itnik <997585659@qq.com>
2016-08-12 09:16:51 +0800

    添加更多的属性配置

--- vendor/config.js
+++ vendor/config.js
@@ -1,19 +1,27 @@
 var jquery_ztree_toc_opts = {
-    debug:false,
-    is_auto_number:true,
-    documment_selector:'.markdown-body',
+    //是否开启DEBUG
+    debug: false,
+    //是否显示Header编号
+    is_auto_number: true,
+    //是否展开所有节点
+    is_expand_all: true,
+    //是否对选中行显示高亮效果
+    is_highlight_selected_line: true,
+    _header_nodes_name:'API',
+    //扫描header的根dom
+    documment_selector: '.markdown-body',
     ztreeStyle: {
-        width:'326px',
+        width: '326px',
         overflow: 'auto',
         position: 'fixed',
         'z-index': 2147483647,
         border: '0px none',
         left: '0px',
         top: '0px',
-		'height': $(window).height() + 'px'
+        'height': $(window).height() + 'px'
     }
-}
+};
 var markdown_panel_style = {
-    'width':'70%',
-    'margin-left':'25%'
+    'width': '70%',
+    'margin-left': '25%'
 };
